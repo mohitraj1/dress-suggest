806eb92758c81cfe2d867f5899bc0a67694a3675
mohitraj1 <mohitraj@gmail.com>
2016-12-29 16:16:24 +0530

    Added logs

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+console.log("DRESS log - reached point 1");
 var express = require('express');
+console.log("DRESS log - reached point 2");
 var app = express();
 
+console.log("DRESS log - reached point 3");
 app.get('/hello', function(req, res) {
 	console.log("request received");
 	var data= [{'name':'AE Checks','image':'Picture1.jpg','outOfService':0},{'name':'Bossni','image':'Picture2.jpg','outOfService':0},{'name':'Gucci','image':'Picture3.jpg','outOfService':0}];//data;
@@ -11,6 +14,7 @@ app.get('/hello', function(req, res) {
      // res.end();
 });
 
+console.log("DRESS log - reached point 4");
 app.listen(process.env.PORT || 3000, function(){
 	console.log("Server started at port 3000");
 });
